f8f6bb26b44cb35f45e5b10851e50ae6ee13d155
mushood <mushood.badulla@gmail.com>
2018-01-27 15:51:05 +0400

    Vue JS admin setup

--- resources/assets/js/admin.js
+++ resources/assets/js/admin.js
@@ -1,1 +1,19 @@
 require('./bootstrap');
+import VueSweetalert2 from 'vue-sweetalert2';
+import { VueEditor } from 'vue2-editor'
+
+window.Vue = require('vue');
+Vue.use(VueSweetalert2);
+Vue.use(VueEditor);
+
+window.Event = new Vue();
+
+Vue.component('multiplefileuploader', require('./components/fileuploader.vue'));
+Vue.component('vueeditora', VueEditor);
+
+const app = new Vue({
+    el: '#admin',
+
+    data:{
+    },
+});
--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -1,6 +1,7 @@
+import Vue from 'vue';
+import axios from 'axios';
 
-window._ = require('lodash');
-
+window.Vue = Vue;
 /**
  * We'll load jQuery and the Bootstrap jQuery plugin which provides support
  * for JavaScript based Bootstrap features such as modals and tabs. This
@@ -36,20 +37,3 @@ if (token) {
 } else {
     console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
 }
-
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allows your team to easily build robust real-time web applications.
- */
-
-// import Echo from 'laravel-echo'
-
-// window.Pusher = require('pusher-js');
-
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: 'your-pusher-key',
-//     cluster: 'mt1',
-//     encrypted: true
-// });
