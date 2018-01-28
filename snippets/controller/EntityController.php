47964433504874fa07c0b75490469e4d026962e3
mushood <mushood.badulla@gmail.com>
2018-01-28 18:23:00 +0400

    correcting entity controller

--- snippets/controller/EntityController.php
+++ snippets/controller/EntityController.php
@@ -4,9 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Image;
-use Carbon\Carbon;
 
-class AdminEntityController extends Controller
+class EntityController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -15,9 +14,9 @@ class AdminEntityController extends Controller
      */
     public function index()
     {
-      $entitys = Entity::latest()->with('image')->paginate(12);
+      $entitys = Entity::latest()->where('active',true)->with('image')->paginate(12);
 
-      return view('admin.entity.index', compact('entitys'));
+      return view('entity.index', compact('entitys'));
     }
 
     /**
@@ -29,7 +28,7 @@ class AdminEntityController extends Controller
     public function show(Entity $entity)
     {
 
-        return view('admin.entity.show', compact('entity'));
+        return view('entity.show', compact('entity'));
     }
 
 }
