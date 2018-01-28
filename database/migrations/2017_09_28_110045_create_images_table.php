853b897ea9fb0d9060559ec6ad7a6d780924baaa
mushood <mushood.badulla@gmail.com>
2018-01-28 19:30:58 +0400

    adding timestamps to images

--- database/migrations/2017_09_28_110045_create_images_table.php
+++ database/migrations/2017_09_28_110045_create_images_table.php
@@ -14,9 +14,10 @@ class CreateImagesTable extends Migration
     public function up()
     {
         Schema::create('images', function (Blueprint $table) {
-          $table->increments('id');
-          $table->string('name');
-          $table->string('alt');
+            $table->increments('id');
+            $table->string('name');
+            $table->string('alt');
+            $table->timestamps();
         });
     }
 
