6b5539003bc320446caf3bccd976160cd6403c9f
mushood <mushood.badulla@gmail.com>
2018-01-29 21:34:44 +0400

    correcting front view show

--- snippets/views/entity/front/show.vue
+++ snippets/views/entity/front/show.vue
@@ -3,9 +3,9 @@
     <div class="row">
         <div class="col-md-12">
             <h3 v-if="entity.title">{{entity.title}}</h3>
-            <p v-if="entity.body" v-html="entity.body"></p>
             <img :src="route_image + '/' + entity.image.name" class="img-responsive col-md-10" alt="image" v-if="entity.image.name"/>
-            <a class="btn btn-success" :href="route_back">Back</a>
+            <p v-if="entity.body" v-html="entity.body"></p>
+            <a class="btn btn-success btn-block" :href="route_back">Back</a>
         </div>
     </div>
 </div>
