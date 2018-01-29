160dc75980c86a763aa8b0181b9844b8be4cd541
mushood <mushood.badulla@gmail.com>
2018-01-29 21:33:40 +0400

    correcting images folder link

--- snippets/views/entity/front/show.blade.php
+++ snippets/views/entity/front/show.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <entity
-        route_image="{{url('/images/entity')}}"
+        route_image="{{url('/images/entitys')}}"
         route_back="{{route('entity.front.index')}}"
         :entity="{{$entity}}"
     ></entity>
