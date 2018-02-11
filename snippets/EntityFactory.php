980a6c3a6a2f2a22106c063cd775cc2a859155d3
mushood <mushood.badulla@gmail.com>
2018-02-11 08:53:53 +0400

    slug correction

--- snippets/EntityFactory.php
+++ snippets/EntityFactory.php
@@ -3,10 +3,12 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Models\Entity::class, function (Faker $faker) {
+    $title = $faker->word;
     return [
-        'title' => $faker->words,
+        'title' => $title,
         'body' => $faker->paragraph,
         'image_id' => 1,
         'order' => 0,
+        'slug' => $title,
     ];
 });
